c98960f4d89fffda82675a16558a15669a364027
Igor Vinícius <116587391+igorviniciusfd@users.noreply.github.com>
2024-03-06 16:32:29 -0300

    Commit Inicial

---
+++ testeEstagio/src/testeEstagio/TesteSoma.java
@@ -0,0 +1,15 @@
+package testeEstagio;
+
+public class TesteSoma {
+
+	public static void main(String[] args) {
+		int Indice = 13, soma = 0, K = 0;
+		while (K < Indice) {
+			K += 1;
+			soma += K;
+			System.out.println(soma);
+		}
+
+	}
+
+}
